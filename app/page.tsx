83bb8e6992de894df0d6f8206338208c5535ca92
kaetsevolibackwards <ericgarmand@gmail.com>
2026-07-10 15:11:11 -0500

    Push updated page.tsx and all remaining game files (Pac-Man, Space Invaders, Minesweeper, Hangman)

--- app/page.tsx
+++ app/page.tsx
@@ -4,133 +4,18 @@ import { useEffect, useState } from 'react'
 import styles from './page.module.css'
 
 const GAMES = [
-  // Direct playable games
-  {
-    title: 'Flappy Bird',
-    url: 'https://nebez.github.io/flappybird/',
-  },
-  {
-    title: '2048',
-    url: 'https://gabrielecirulli.github.io/2048/',
-  },
-  {
-    title: 'Tetris',
-    url: 'https://chvin.github.io/react-tetris/',
-  },
-  {
-    title: 'Snake Game',
-    url: 'https://anotak.github.io/slither/',
-  },
-  {
-    title: 'Pac-Man',
-    url: 'https://plyr.link/pac-man/',
-  },
-  {
-    title: 'Space Invaders',
-    url: 'https://plyr.link/space-invaders/',
-  },
-  {
-    title: 'Asteroids',
-    url: 'https://plyr.link/asteroids/',
-  },
-  {
-    title: 'Chess',
-    url: 'https://www.chess.com/play/online',
-  },
-  {
-    title: 'Checkers',
-    url: 'https://www.gamesforthebrain.com/game/checkers/',
-  },
-  {
-    title: 'Tic Tac Toe',
-    url: 'https://www.gamesforthebrain.com/game/tictactoe/',
-  },
-  {
-    title: 'Hangman',
-    url: 'https://www.gamesforthebrain.com/game/hangman/',
-  },
-  {
-    title: 'Cookie Clicker',
-    url: 'https://orteil.dashnet.org/cookieclicker/',
-  },
-  {
-    title: 'Wordle',
-    url: 'https://www.powerlanguage.co.uk/wordle/',
-  },
-  {
-    title: 'Minesweeper',
-    url: 'https://minesweeper.online/',
-  },
-  {
-    title: 'Sudoku',
-    url: 'https://www.sudokuplay.com/',
-  },
-  {
-    title: 'Portal Flash',
-    url: 'https://www.addictinggames.com/adventure/portal-flash-version',
-  },
-  {
-    title: 'Dino Chrome',
-    url: 'https://chromedino.com/',
-  },
-  {
-    title: 'Agar.io',
-    url: 'https://agar.io/',
-  },
-  {
-    title: 'Slither.io',
-    url: 'https://slither.io/',
-  },
-  {
-    title: 'Diep.io',
-    url: 'https://diep.io/',
-  },
-  // GitHub games that work
-  {
-    title: 'Doom JS',
-    url: 'https://js-dos.com/games/doom',
-  },
-  {
-    title: 'Sokoban',
-    url: 'https://www.gamesforthebrain.com/game/sokoban/',
-  },
-  {
-    title: 'Simon Says',
-    url: 'https://www.gamesforthebrain.com/game/simon/',
-  },
-  {
-    title: 'Memory Game',
-    url: 'https://www.gamesforthebrain.com/game/memory/',
-  },
-  {
-    title: 'Maze Game',
-    url: 'https://www.gamesforthebrain.com/game/maze/',
-  },
-  {
-    title: 'Connect 4',
-    url: 'https://www.gamesforthebrain.com/game/connect4/',
-  },
-  {
-    title: 'Yahtzee',
-    url: 'https://www.gamesforthebrain.com/game/yahtzee/',
-  },
-  {
-    title: 'Blackjack',
-    url: 'https://www.gamesforthebrain.com/game/blackjack/',
-  },
-  // Proxy sites
-  {
-    title: 'CroxyProxy',
-    url: 'https://www.croxyproxy.com/',
-  },
-  {
-    title: 'ProxySite',
-    url: 'https://www.proxysite.com/',
-  },
-  {
-    title: 'Unblocker',
-    url: 'https://unblocker.yt/',
-  },
+  { title: 'Eaglercraft', path: '/games/eaglercraft.html' },
+  { title: '2048', path: '/games/2048.html' },
+  { title: 'Flappy Bird', path: '/games/flappy-bird.html' },
+  { title: 'Snake', path: '/games/snake.html' },
+  { title: 'Tetris', path: '/games/tetris.html' },
+  { title: 'Memory Match', path: '/games/memory.html' },
+  { title: 'Tic Tac Toe', path: '/games/tictactoe.html' },
+  { title: 'Pac-Man', path: '/games/pacman.html' },
+  { title: 'Space Invaders', path: '/games/space-invaders.html' },
+  { title: 'Minesweeper', path: '/games/minesweeper.html' },
+  { title: 'Cookie Clicker', path: '/games/cookie-clicker.html' },
+  { title: 'Hangman', path: '/games/hangman.html' },
 ]
 
 export default function Home() {
@@ -155,11 +40,10 @@ export default function Home() {
           ← Back
         </button>
         <iframe
-          src={selectedGame.url}
+          src={selectedGame.path}
           title={selectedGame.title}
           className={styles.gameFrame}
-          allow="autoplay; fullscreen"
-          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+          sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals"
         />
       </div>
     )
